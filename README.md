4e6cad938ef62f5c6fe4775dba196ca3c418c8ef
Nathanael Jones <nathanael.jones@gmail.com>
2015-05-27 14:38:17 -0400

    Add sample project and usage info to readme

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ master: [![Build status](https://ci.appveyor.com/api/projects/status/12bkj9y5wcy
 C# port of the [excellent Java MetadataExtractor library by Drew Noakes](https://drewnoakes.com/code/exif/). 
 
 
+
+
 Initial coversion was performed with [Sharpen](https://github.com/imazen/sharpen); details can be found in [HowToConvert.md](HowToConvert.md) and [SharpenBugsAndProblems.md](SharpenBugsAndProblems.md). Significant manual work was required afterward to make the library build and pass all tests correctly.
 
 Conversion was performed against [this copy](https://github.com/ydanila/j-metadata-extractor) of the MetadataExtractor library, which [was reorganized to be amenable to Sharpen conversion](https://github.com/ydanila/j-metadata-extractor/commit/0b6d857dde184bf992a975957521f950ed0e92f6). This copy, in turn, was based on the [Jan 18, 2014 commit eb70b234529a](https://code.google.com/p/metadata-extractor/source/detail?r=eb70b234529ae267c9ba72e9df68d9acb7e3504b). 
@@ -16,10 +18,33 @@ Many thanks to Yakov Danilov <yakodani@gmail.com> his work in porting this libra
 
 Also, special thanks to [Ferret Renaud, who provided a C# port for many years](http://ferretrenaud.fr/Projets/MetaDataExtractor/index.html). His code can be found in the 'renaud' branch. 
 
-All code herein is licensed under one of the following licenses: Apache 2, BSD, MIT. See LICENSE for details about which code is licensed under which license.
+All code herein is licensed under one of the following licenses: Apache 2, BSD, MIT. See LICENSE for details about which code is licensed under which license. 
+
+ICSharpCode.SharpZipLib is licensed under the GPL with a linking exception, so it can be used in commerical products. 
 
 
 ### To-do
 
 * Publish to NuGet
 * Add documentation (and where possible, re-use documentation from the original).
+
+## Example use
+
+Autocoverted examples: 
+
+https://github.com/imazen/n-metadata-extractor/blob/master/Com.Drew/Com/drew/tools/ProcessAllImagesInFolderUtility.cs
+
+https://github.com/imazen/n-metadata-extractor/blob/master/Com.Drew/Com/drew/tools/ProcessUrlUtility.cs
+
+Sample project "SampleReader"
+
+
+### Basic usage
+
+```
+
+var meta = ImageMetadataReader.ReadMetadata(InputStream.Wrap(stream)); //Stream must be seekable
+
+//Produce a list of strings containing metadata key/value pairs. 
+var strings = metadata.GetDirectories().SelectMany(d => d.GetTags().Select(t => String.Format("[{0}] {1} = {2}\n",d.GetName(), t.GetTagName(), t.GetDescription()))).ToList();
+```
---
+++ SampleReader/Program.cs
@@ -0,0 +1,123 @@
+﻿using Com.Drew.Imaging;
+using Sharpen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleReader
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var resource = "https://raw.githubusercontent.com/drewnoakes/metadata-extractor-images/master/jpg/Apple%20iPhone%206%20plus.jpg";
+            var request = HttpWebRequest.Create(new Uri(resource));
+            var exit_code = 0;
+
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream()){
+                var meta = ParseMetadata(stream, resource);
+                if (meta != null)
+                {
+                    var success = PrintMetadata(meta);
+                    if (!success)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        //Some metadata bits might be corrupt
+                        Console.Error.WriteLine("Some invalid metadata was detected.");
+                        exit_code = 2;
+                    }
+                    else
+                    {
+                        exit_code = 0; //All peachy!
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    //This might not be an image, or it might be badly corrupted, or unsupported.
+                    Console.WriteLine("This is not a valid image, or is not a type supported by MetadataExtractor");
+                    exit_code =  1;
+                }
+
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();
+            return exit_code;
+           
+
+        }
+        /// <summary>
+        /// Parses the metadata, writing error information to the console if it failes.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        static Com.Drew.Metadata.Metadata  ParseMetadata(Stream data, string info){
+
+            try
+			{
+                if (!data.CanSeek)
+                {
+                    var ms = new MemoryStream(4096);//We have to guess at length, can't seek
+                    data.CopyTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    return ImageMetadataReader.ReadMetadata(InputStream.Wrap(ms));
+                }
+                return ImageMetadataReader.ReadMetadata(InputStream.Wrap(data));
+			}
+			catch (ImageProcessingException e)
+			{
+                Console.ForegroundColor = ConsoleColor.Red;
+				// this is an error in the Jpeg segment structure.  we're looking for bad handling of
+				// metadata segments.  in this case, we didn't even get a segment.
+				Console.Error.WriteLine("{0}: {1} [Error Extracting Metadata]\n\t{2}\n", e.GetType().FullName, info, e.Message);
+				return null;
+			}
+			catch (Exception t)
+			{
+                Console.ForegroundColor = ConsoleColor.Red;
+				// general, uncaught exception during processing of jpeg segments
+				Console.Error.WriteLine("{0}: {1} [Error Extracting Metadata]\n", t.GetType().FullName, info);
+                Console.Error.WriteLine(t);
+				return null;
+			}
+        }
+
+        static bool PrintMetadata(Com.Drew.Metadata.Metadata metadata){
+            
+            foreach (Com.Drew.Metadata.Directory dir in metadata.GetDirectories())
+			{
+                if (dir.HasErrors())
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    foreach (string error in dir.GetErrors())
+                    {
+                        Console.Error.WriteLine("\t[{0}] {1}\n", dir.GetName(), error);
+                    }
+                }
+              
+				foreach (Com.Drew.Metadata.Tag tag in dir.GetTags())
+				{
+					string tagName = tag.GetTagName();
+					string directoryName = dir.GetName();
+					string description = tag.GetDescription(); 
+					// truncate the description if it's too long
+					if (description != null && description.Length > 1024)
+					{
+						description = description.Substring(0,1024)  + "...";
+					}
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[{0}] {1} = {2}\n", directoryName, tagName, description);
+				}
+
+			}
+            
+            return metadata.GetDirectories().Any(d => d.HasErrors()); //True if there are any metadata errors;
+        }
+
+    }
+}
