94b10b0f82e0a0fb0084903e57e40b12b1233cf2
Drew Noakes <git@drewnoakes.com>
2015-06-09 00:31:55 +0100

    Code formatting.

--- Sharpen/OutputStreamWriter.cs
+++ Sharpen/OutputStreamWriter.cs
@@ -4,11 +4,13 @@ namespace Sharpen
 {
     public class OutputStreamWriter : StreamWriter
     {
-        public OutputStreamWriter (OutputStream stream, string encoding) : base(stream.GetWrappedStream (), Extensions.GetEncoding (encoding))
+        public OutputStreamWriter (OutputStream stream, string encoding)
+            : base(stream.GetWrappedStream (), Extensions.GetEncoding (encoding))
         {
         }
 
-        public OutputStreamWriter(PrintStream printStream) : base(printStream.InternalWriter.BaseStream)
+        public OutputStreamWriter(PrintStream printStream)
+            : base(printStream.InternalWriter.BaseStream)
         {
         }
     }
