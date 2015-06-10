66ec2c350e04a2482899be8206fdb5e884e5287e
Drew Noakes <git@drewnoakes.com>
2015-06-10 16:51:31 +0100

    Make SequentialReader methods non-virtual.

--- Com.Drew/Com/drew/lang/SequentialReader.cs
+++ Com.Drew/Com/drew/lang/SequentialReader.cs
@@ -71,7 +71,7 @@ namespace Com.Drew.Lang
         /// </list>
         /// </remarks>
         /// <param name="motorolaByteOrder"><c>true</c> for Motorola/big endian, <c>false</c> for Intel/little endian</param>
-        public virtual void SetMotorolaByteOrder(bool motorolaByteOrder)
+        public void SetMotorolaByteOrder(bool motorolaByteOrder)
         {
             _isMotorolaByteOrder = motorolaByteOrder;
         }
@@ -84,7 +84,7 @@ namespace Com.Drew.Lang
         /// <item><c>false</c> for Intel (or little) endianness, with LSB before MSB.</item>
         /// </list>
         /// </remarks>
-        public virtual bool IsMotorolaByteOrder()
+        public bool IsMotorolaByteOrder()
         {
             return _isMotorolaByteOrder;
         }
@@ -92,7 +92,7 @@ namespace Com.Drew.Lang
         /// <summary>Returns an unsigned 8-bit int calculated from the next byte of the sequence.</summary>
         /// <returns>the 8 bit int value, between 0 and 255</returns>
         /// <exception cref="System.IO.IOException"/>
-        public virtual short GetUInt8()
+        public short GetUInt8()
         {
             return (short)(GetByte() & unchecked(0xFF));
         }
@@ -100,7 +100,7 @@ namespace Com.Drew.Lang
         /// <summary>Returns a signed 8-bit int calculated from the next byte the sequence.</summary>
         /// <returns>the 8 bit int value, between 0x00 and 0xFF</returns>
         /// <exception cref="System.IO.IOException"/>
-        public virtual byte GetInt8()
+        public byte GetInt8()
         {
             return GetByte();
         }
@@ -108,7 +108,7 @@ namespace Com.Drew.Lang
         /// <summary>Returns an unsigned 16-bit int calculated from the next two bytes of the sequence.</summary>
         /// <returns>the 16 bit int value, between 0x0000 and 0xFFFF</returns>
         /// <exception cref="System.IO.IOException"/>
-        public virtual int GetUInt16()
+        public int GetUInt16()
         {
             if (_isMotorolaByteOrder)
             {
@@ -122,7 +122,7 @@ namespace Com.Drew.Lang
         /// <summary>Returns a signed 16-bit int calculated from two bytes of data (MSB, LSB).</summary>
         /// <returns>the 16 bit int value, between 0x0000 and 0xFFFF</returns>
         /// <exception cref="System.IO.IOException">the buffer does not contain enough bytes to service the request</exception>
-        public virtual short GetInt16()
+        public short GetInt16()
         {
             if (_isMotorolaByteOrder)
             {
@@ -136,7 +136,7 @@ namespace Com.Drew.Lang
         /// <summary>Get a 32-bit unsigned integer from the buffer, returning it as a long.</summary>
         /// <returns>the unsigned 32-bit int value as a long, between 0x00000000 and 0xFFFFFFFF</returns>
         /// <exception cref="System.IO.IOException">the buffer does not contain enough bytes to service the request</exception>
-        public virtual long GetUInt32()
+        public long GetUInt32()
         {
             if (_isMotorolaByteOrder)
             {
@@ -150,7 +150,7 @@ namespace Com.Drew.Lang
         /// <summary>Returns a signed 32-bit integer from four bytes of data.</summary>
         /// <returns>the signed 32 bit int value, between 0x00000000 and 0xFFFFFFFF</returns>
         /// <exception cref="System.IO.IOException">the buffer does not contain enough bytes to service the request</exception>
-        public virtual int GetInt32()
+        public int GetInt32()
         {
             if (_isMotorolaByteOrder)
             {
@@ -164,7 +164,7 @@ namespace Com.Drew.Lang
         /// <summary>Get a signed 64-bit integer from the buffer.</summary>
         /// <returns>the 64 bit int value, between 0x0000000000000000 and 0xFFFFFFFFFFFFFFFF</returns>
         /// <exception cref="System.IO.IOException">the buffer does not contain enough bytes to service the request</exception>
-        public virtual long GetInt64()
+        public long GetInt64()
         {
             if (_isMotorolaByteOrder)
             {
@@ -184,7 +184,7 @@ namespace Com.Drew.Lang
         /// </remarks>
         /// <returns>the floating point value</returns>
         /// <exception cref="System.IO.IOException">the buffer does not contain enough bytes to service the request</exception>
-        public virtual float GetS15Fixed16()
+        public float GetS15Fixed16()
         {
             if (_isMotorolaByteOrder)
             {
@@ -202,27 +202,27 @@ namespace Com.Drew.Lang
         }
 
         /// <exception cref="System.IO.IOException"/>
-        public virtual float GetFloat32()
+        public float GetFloat32()
         {
             return BitConverter.ToSingle(BitConverter.GetBytes(GetInt32()), 0);
         }
 
         /// <exception cref="System.IO.IOException"/>
-        public virtual double GetDouble64()
+        public double GetDouble64()
         {
             return BitConverter.Int64BitsToDouble(GetInt64());
         }
 
         /// <exception cref="System.IO.IOException"/>
         [NotNull]
-        public virtual string GetString(int bytesRequested)
+        public string GetString(int bytesRequested)
         {
             return GetString(bytesRequested, Encoding.UTF8);
         }
 
         /// <exception cref="System.IO.IOException"/>
         [NotNull]
-        public virtual string GetString(int bytesRequested, Encoding encoding)
+        public string GetString(int bytesRequested, Encoding encoding)
         {
             return encoding.GetString(GetBytes(bytesRequested));
         }
@@ -235,13 +235,13 @@ namespace Com.Drew.Lang
         /// <returns>The read string.</returns>
         /// <exception cref="System.IO.IOException">The buffer does not contain enough bytes to satisfy this request.</exception>
         [NotNull]
-        public virtual string GetNullTerminatedString(int maxLengthBytes)
+        public string GetNullTerminatedString(int maxLengthBytes)
         {
             // NOTE currently only really suited to single-byte character strings
             var bytes = new byte[maxLengthBytes];
             // Count the number of non-null bytes
             var length = 0;
-            while (length < bytes.Length && (bytes[length] = GetByte()) != '\0')
+            while (length < bytes.Length && (bytes[length] = GetByte()) != 0)
             {
                 length++;
             }
