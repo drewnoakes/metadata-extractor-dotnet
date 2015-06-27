62ff3cf83cbc0c470b49fc9906c3c767ba8d51c1
Drew Noakes <git@drewnoakes.com>
2015-06-27 20:32:01 +0100

    Format code in XmpDescriptor.

--- MetadataExtractor/Formats/Xmp/XmpDescriptor.cs
+++ MetadataExtractor/Formats/Xmp/XmpDescriptor.cs
@@ -28,11 +28,7 @@ using MetadataExtractor.Util;
 
 namespace MetadataExtractor.Formats.Xmp
 {
-    /// <summary>Contains all logic for the presentation of xmp data, as stored in Xmp-Segment.</summary>
-    /// <remarks>
-    /// Contains all logic for the presentation of xmp data, as stored in Xmp-Segment.  Use
-    /// this class to provide human-readable descriptions of tag values.
-    /// </remarks>
+    /// <summary>Contains logic for the presentation of data stored in an <see cref="XmpDirectory"/>.</summary>
     /// <author>Torsten Skadell, Drew Noakes https://drewnoakes.com</author>
     public sealed class XmpDescriptor : TagDescriptor<XmpDirectory>
     {
@@ -42,142 +38,71 @@ namespace MetadataExtractor.Formats.Xmp
         }
 
         // TODO some of these methods look similar to those found in Exif*Descriptor... extract common functionality from both
-        /// <summary>Do some simple formatting, dependant upon tagType</summary>
+
+        /// <summary>Do some simple formatting, dependent upon <paramref name="tagType"/>.</summary>
         public override string GetDescription(int tagType)
         {
             switch (tagType)
             {
                 case XmpDirectory.TagMake:
                 case XmpDirectory.TagModel:
-                {
                     return Directory.GetString(tagType);
-                }
-
                 case XmpDirectory.TagExposureTime:
-                {
                     return GetExposureTimeDescription();
-                }
-
                 case XmpDirectory.TagExposureProgram:
-                {
                     return GetExposureProgramDescription();
-                }
-
                 case XmpDirectory.TagShutterSpeed:
-                {
                     return GetShutterSpeedDescription();
-                }
-
                 case XmpDirectory.TagFNumber:
-                {
                     return GetFNumberDescription();
-                }
-
                 case XmpDirectory.TagLens:
                 case XmpDirectory.TagLensInfo:
                 case XmpDirectory.TagCameraSerialNumber:
                 case XmpDirectory.TagFirmware:
-                {
                     return Directory.GetString(tagType);
-                }
-
                 case XmpDirectory.TagFocalLength:
-                {
                     return GetFocalLengthDescription();
-                }
-
                 case XmpDirectory.TagApertureValue:
-                {
                     return GetApertureValueDescription();
-                }
-
                 default:
-                {
                     return base.GetDescription(tagType);
-                }
             }
         }
 
-        /// <summary>Do a simple formatting like ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetExposureTimeDescription()
         {
             var value = Directory.GetString(XmpDirectory.TagExposureTime);
             if (value == null)
-            {
                 return null;
-            }
             return value + " sec";
         }
 
-        /// <summary>This code is from ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetExposureProgramDescription()
         {
-            // '1' means manual control, '2' program normal, '3' aperture priority,
-            // '4' shutter priority, '5' program creative (slow program),
-            // '6' program action(high-speed program), '7' portrait mode, '8' landscape mode.
             int value;
             if (!Directory.TryGetInt32(XmpDirectory.TagExposureProgram, out value))
                 return null;
-            switch (value)
-            {
-                case 1:
-                {
-                    return "Manual control";
-                }
 
-                case 2:
-                {
-                    return "Program normal";
-                }
-
-                case 3:
-                {
-                    return "Aperture priority";
-                }
-
-                case 4:
-                {
-                    return "Shutter priority";
-                }
-
-                case 5:
-                {
-                    return "Program creative (slow program)";
-                }
-
-                case 6:
-                {
-                    return "Program action (high-speed program)";
-                }
-
-                case 7:
-                {
-                    return "Portrait mode";
-                }
-
-                case 8:
-                {
-                    return "Landscape mode";
-                }
-
-                default:
-                {
-                    return "Unknown program (" + value + ")";
-                }
-            }
+            return GetIndexedDescription(XmpDirectory.TagExposureProgram, 1,
+                "Manual control",
+                "Program normal",
+                "Aperture priority",
+                "Shutter priority",
+                "Program creative (slow program)",
+                "Program action (high-speed program)",
+                "Portrait mode",
+                "Landscape mode");
         }
 
-        /// <summary>This code is from ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetShutterSpeedDescription()
         {
             var value = Directory.GetSingleNullable(XmpDirectory.TagShutterSpeed);
             if (value == null)
-            {
                 return null;
-            }
+
             // thanks to Mark Edwards for spotting and patching a bug in the calculation of this
             // description (spotted bug using a Canon EOS 300D)
             // thanks also to Gli Blr for spotting this bug
@@ -195,19 +120,15 @@ namespace MetadataExtractor.Formats.Xmp
             }
         }
 
-        /// <summary>Do a simple formatting like ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetFNumberDescription()
         {
             var value = Directory.GetRational(XmpDirectory.TagFNumber);
             if (value == null)
-            {
                 return null;
-            }
             return "f/" + value.ToDouble().ToString("0.0");
         }
 
-        /// <summary>This code is from ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetFocalLengthDescription()
         {
@@ -215,7 +136,6 @@ namespace MetadataExtractor.Formats.Xmp
             return value == null ? null : string.Format("{0:0.0##} mm", value.ToDouble());
         }
 
-        /// <summary>This code is from ExifSubIFDDescriptor.java</summary>
         [CanBeNull]
         public string GetApertureValueDescription()
         {
