80d033d8ee8c9dbf3f05bca1e473e06407098f87
Drew Noakes <git@drewnoakes.com>
2015-06-28 13:57:47 +0100

    Formatting code in PhotoshopDescriptor.

--- MetadataExtractor/Formats/Photoshop/PhotoshopDescriptor.cs
+++ MetadataExtractor/Formats/Photoshop/PhotoshopDescriptor.cs
@@ -22,8 +22,6 @@
 //
 #endregion
 
-using System;
-using System.IO;
 using System.Text;
 using JetBrains.Annotations;
 using MetadataExtractor.IO;
@@ -45,68 +43,33 @@ namespace MetadataExtractor.Formats.Photoshop
             {
                 case PhotoshopDirectory.TagThumbnail:
                 case PhotoshopDirectory.TagThumbnailOld:
-                {
                     return GetThumbnailDescription(tagType);
-                }
-
                 case PhotoshopDirectory.TagUrl:
                 case PhotoshopDirectory.TagXml:
-                {
                     return GetSimpleString(tagType);
-                }
-
                 case PhotoshopDirectory.TagIptc:
-                {
                     return GetBinaryDataString(tagType);
-                }
-
                 case PhotoshopDirectory.TagSlices:
-                {
                     return GetSlicesDescription();
-                }
-
                 case PhotoshopDirectory.TagVersion:
-                {
                     return GetVersionDescription();
-                }
-
                 case PhotoshopDirectory.TagCopyright:
-                {
                     return GetBooleanString(tagType);
-                }
-
                 case PhotoshopDirectory.TagResolutionInfo:
-                {
                     return GetResolutionInfoDescription();
-                }
-
                 case PhotoshopDirectory.TagGlobalAngle:
                 case PhotoshopDirectory.TagGlobalAltitude:
                 case PhotoshopDirectory.TagUrlList:
                 case PhotoshopDirectory.TagSeedNumber:
-                {
                     return Get32BitNumberString(tagType);
-                }
-
                 case PhotoshopDirectory.TagJpegQuality:
-                {
                     return GetJpegQualityString();
-                }
-
                 case PhotoshopDirectory.TagPrintScale:
-                {
                     return GetPrintScaleDescription();
-                }
-
                 case PhotoshopDirectory.TagPixelAspectRatio:
-                {
                     return GetPixelAspectRatioString();
-                }
-
                 default:
-                {
                     return base.GetDescription(tagType);
-                }
             }
         }
 
@@ -116,30 +79,22 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var b = Directory.GetByteArray(PhotoshopDirectory.TagJpegQuality);
+
                 if (b == null)
-                {
                     return Directory.GetString(PhotoshopDirectory.TagJpegQuality);
-                }
-                IndexedReader reader = new ByteArrayReader(b);
+
+                var reader = new ByteArrayReader(b);
+
                 int q = reader.GetUInt16(0);
                 int f = reader.GetUInt16(2);
                 int s = reader.GetUInt16(4);
-                int q1;
-                if (q <= 0xFFFF && q >= 0xFFFD)
-                {
-                    q1 = q - 0xFFFC;
-                }
-                else
-                {
-                    if (q <= 8)
-                    {
-                        q1 = q + 4;
-                    }
-                    else
-                    {
-                        q1 = q;
-                    }
-                }
+
+                var q1 = q <= 0xFFFF && q >= 0xFFFD
+                    ? q - 0xFFFC
+                    : q <= 8
+                        ? q + 4
+                        : q;
+
                 string quality;
                 switch (q)
                 {
@@ -147,71 +102,51 @@ namespace MetadataExtractor.Formats.Photoshop
                     case 0xFFFE:
                     case 0xFFFF:
                     case 0:
-                    {
                         quality = "Low";
                         break;
-                    }
-
                     case 1:
                     case 2:
                     case 3:
-                    {
                         quality = "Medium";
                         break;
-                    }
-
                     case 4:
                     case 5:
-                    {
                         quality = "High";
                         break;
-                    }
-
                     case 6:
                     case 7:
                     case 8:
-                    {
                         quality = "Maximum";
                         break;
-                    }
-
                     default:
-                    {
                         quality = "Unknown";
                         break;
-                    }
                 }
+
                 string format;
                 switch (f)
                 {
                     case 0x0000:
-                    {
                         format = "Standard";
                         break;
-                    }
-
                     case 0x0001:
-                    {
                         format = "Optimised";
                         break;
-                    }
-
                     case 0x0101:
-                    {
                         format = "Progressive ";
                         break;
-                    }
-
                     default:
-                    {
                         format = string.Format("Unknown (0x{0:X4})", f);
                         break;
-                    }
                 }
-                var scans = s >= 1 && s <= 3 ? (s + 2).ToString() : string.Format("Unknown (0x{0:X4})", s);
+
+                var scans = s >= 1 && s <= 3
+                    ? (s + 2).ToString()
+                    : string.Format("Unknown (0x{0:X4})", s);
+
                 return string.Format("{0} ({1}), {2} format, {3} scans", q1, quality, format, scans);
             }
-            catch (IOException)
+            catch
             {
                 return null;
             }
@@ -223,15 +158,15 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var bytes = Directory.GetByteArray(PhotoshopDirectory.TagPixelAspectRatio);
+
                 if (bytes == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(bytes);
+
+                var reader = new ByteArrayReader(bytes);
                 var d = reader.GetDouble64(4);
-                return ((object)d).ToString();
+                return d.ToString();
             }
-            catch (Exception)
+            catch
             {
                 return null;
             }
@@ -243,39 +178,29 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var bytes = Directory.GetByteArray(PhotoshopDirectory.TagPrintScale);
+
                 if (bytes == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(bytes);
+
+                var reader = new ByteArrayReader(bytes);
                 var style = reader.GetInt32(0);
                 var locX = reader.GetFloat32(2);
                 var locY = reader.GetFloat32(6);
                 var scale = reader.GetFloat32(10);
+
                 switch (style)
                 {
                     case 0:
-                    {
                         return "Centered, Scale " + scale;
-                    }
-
                     case 1:
-                    {
                         return "Size to fit";
-                    }
-
                     case 2:
-                    {
                         return string.Format("User defined, X:{0} Y:{1}, Scale:{2}", locX, locY, scale);
-                    }
-
                     default:
-                    {
                         return string.Format("Unknown {0:X4}, X:{1} Y:{2}, Scale:{3}", style, locX, locY, scale);
-                    }
                 }
             }
-            catch (Exception)
+            catch
             {
                 return null;
             }
@@ -287,17 +212,19 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var bytes = Directory.GetByteArray(PhotoshopDirectory.TagResolutionInfo);
+
                 if (bytes == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(bytes);
+
+                var reader = new ByteArrayReader(bytes);
+
                 var resX = reader.GetS15Fixed16(0);
                 var resY = reader.GetS15Fixed16(8);
+
                 // is this the correct offset? it's only reading 4 bytes each time
                 return resX + "x" + resY + " DPI";
             }
-            catch (Exception)
+            catch
             {
                 return null;
             }
@@ -309,11 +236,12 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var bytes = Directory.GetByteArray(PhotoshopDirectory.TagVersion);
+
                 if (bytes == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(bytes);
+
+                var reader = new ByteArrayReader(bytes);
+
                 var pos = 0;
                 var ver = reader.GetInt32(0);
                 pos += 4;
@@ -327,9 +255,10 @@ namespace MetadataExtractor.Formats.Photoshop
                 var writerStr = reader.GetString(pos, writerLength * 2, Encoding.BigEndianUnicode);
                 pos += writerLength * 2;
                 var fileVersion = reader.GetInt32(pos);
+
                 return string.Format("{0} ({1}, {2}) {3}", ver, readerStr, writerStr, fileVersion);
             }
-            catch (IOException)
+            catch
             {
                 return null;
             }
@@ -341,27 +270,20 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var bytes = Directory.GetByteArray(PhotoshopDirectory.TagSlices);
+
                 if (bytes == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(bytes);
+
+                var reader = new ByteArrayReader(bytes);
+
                 var nameLength = reader.GetInt32(20);
                 var name = reader.GetString(24, nameLength * 2, Encoding.BigEndianUnicode);
                 var pos = 24 + nameLength * 2;
                 var sliceCount = reader.GetInt32(pos);
-                //pos += 4;
                 return string.Format("{0} ({1},{2},{3},{4}) {5} Slices", name, reader.GetInt32(4), reader.GetInt32(8), reader.GetInt32(12), reader.GetInt32(16), sliceCount);
             }
-            catch (IOException)
+            catch
             {
-            /*for (int i=0;i<sliceCount;i++){
-                pos+=16;
-                int slNameLen=getInt32(b,pos);
-                pos+=4;
-                String slName=new String(b, pos, slNameLen*2,"UTF-16");
-                res+=slName;
-            }*/
                 return null;
             }
         }
@@ -372,30 +294,23 @@ namespace MetadataExtractor.Formats.Photoshop
             try
             {
                 var v = Directory.GetByteArray(tagType);
+
                 if (v == null)
-                {
                     return null;
-                }
-                IndexedReader reader = new ByteArrayReader(v);
-                //int pos = 0;
+
+                var reader = new ByteArrayReader(v);
                 var format = reader.GetInt32(0);
-                //pos += 4;
                 var width = reader.GetInt32(4);
-                //pos += 4;
                 var height = reader.GetInt32(8);
-                //pos += 4;
-                //pos += 4; //skip WidthBytes
+                // skip WidthBytes
                 var totalSize = reader.GetInt32(16);
-                //pos += 4;
                 var compSize = reader.GetInt32(20);
-                //pos += 4;
                 var bpp = reader.GetInt32(24);
-                //pos+=2;
-                //pos+=2; //skip Number of planes
-                //int thumbSize=v.length-pos;
+                // skip Number of planes
+
                 return string.Format("{0}, {1}x{2}, Decomp {3} bytes, {4} bpp, {5} bytes", format == 1 ? "JpegRGB" : "RawRGB", width, height, totalSize, bpp, compSize);
             }
-            catch (IOException)
+            catch
             {
                 return null;
             }
@@ -405,10 +320,10 @@ namespace MetadataExtractor.Formats.Photoshop
         private string GetBooleanString(int tag)
         {
             var bytes = Directory.GetByteArray(tag);
+
             if (bytes == null)
-            {
                 return null;
-            }
+
             return bytes[0] == 0 ? "No" : "Yes";
         }
 
@@ -416,16 +331,17 @@ namespace MetadataExtractor.Formats.Photoshop
         private string Get32BitNumberString(int tag)
         {
             var bytes = Directory.GetByteArray(tag);
+
             if (bytes == null)
-            {
                 return null;
-            }
-            IndexedReader reader = new ByteArrayReader(bytes);
+
+            var reader = new ByteArrayReader(bytes);
+
             try
             {
                 return string.Format("{0}", reader.GetInt32(0));
             }
-            catch (IOException)
+            catch
             {
                 return null;
             }
@@ -435,22 +351,20 @@ namespace MetadataExtractor.Formats.Photoshop
         private string GetSimpleString(int tagType)
         {
             var bytes = Directory.GetByteArray(tagType);
-            if (bytes == null)
-            {
-                return null;
-            }
-            return Encoding.UTF8.GetString(bytes);
+
+            return bytes == null
+                ? null
+                : Encoding.UTF8.GetString(bytes);
         }
 
         [CanBeNull]
         private string GetBinaryDataString(int tagType)
         {
             var bytes = Directory.GetByteArray(tagType);
-            if (bytes == null)
-            {
-                return null;
-            }
-            return string.Format("{0} bytes binary data", bytes.Length);
+
+            return bytes == null
+                ? null
+                : string.Format("{0} bytes binary data", bytes.Length);
         }
     }
 }
