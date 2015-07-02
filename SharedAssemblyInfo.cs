78905c4378883cefc272b377dcfb571e34e90127
Drew Noakes <git@drewnoakes.com>
2015-07-02 21:43:39 +0100

    Introduce SharedAssemblyInfo.cs to set some common assembly attributes.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("MetadataExtractor.Tools.FileProcessor")]
+
+[assembly: AssemblyCompany("Drew Noakes")]
+[assembly: AssemblyCopyright("Copyright © Drew Noakes 2015")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("Release")]
+#endif
