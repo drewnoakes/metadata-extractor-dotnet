04e658ce4198b29e586a7dcdc9bfa0f76476d964
Drew Noakes <git@drewnoakes.com>
2015-07-18 23:04:16 +0100

    Formatting.

--- MetadataExtractor/Formats/Tiff/TiffReader.cs
+++ MetadataExtractor/Formats/Tiff/TiffReader.cs
@@ -29,9 +29,7 @@ using MetadataExtractor.IO;
 namespace MetadataExtractor.Formats.Tiff
 {
     /// <summary>
-    /// Processes TIFF-formatted data, calling into client code via that
-    /// <see cref="ITiffHandler"/>
-    /// interface.
+    /// Processes TIFF-formatted data, calling into client code via that <see cref="ITiffHandler"/> interface.
     /// </summary>
     /// <author>Drew Noakes https://drewnoakes.com</author>
     public static class TiffReader
@@ -61,17 +59,22 @@ namespace MetadataExtractor.Formats.Tiff
             // Check the next two values for correctness.
             int tiffMarker = reader.GetUInt16(2 + tiffHeaderOffset);
             handler.SetTiffMarker(tiffMarker);
+
             var firstIfdOffset = reader.GetInt32(4 + tiffHeaderOffset) + tiffHeaderOffset;
+
             // David Ekholm sent a digital camera image that has this problem
-            // TODO getLength should be avoided as it causes IndexedCapturingReader to read to the end of the stream
+            // TODO calling Length should be avoided as it causes IndexedCapturingReader to read to the end of the stream
             if (firstIfdOffset >= reader.Length - 1)
             {
                 handler.Warn("First IFD offset is beyond the end of the TIFF data segment -- trying default offset");
                 // First directory normally starts immediately after the offset bytes, so try that
                 firstIfdOffset = tiffHeaderOffset + 2 + 2 + 4;
             }
-            ICollection<int> processedIfdOffsets = new HashSet<int>();
+
+            var processedIfdOffsets = new HashSet<int>();
+
             ProcessIfd(handler, reader, processedIfdOffsets, firstIfdOffset, tiffHeaderOffset);
+
             handler.Completed(reader, tiffHeaderOffset);
         }
 
@@ -79,14 +82,14 @@ namespace MetadataExtractor.Formats.Tiff
         /// <remarks>
         /// IFD Header:
         /// <list type="bullet">
-        /// <item><b>2 bytes</b> number of tags</item>
+        ///   <item><b>2 bytes</b> number of tags</item>
         /// </list>
         /// Tag structure:
         /// <list type="bullet">
-        /// <item><b>2 bytes</b> tag type</item>
-        /// <item><b>2 bytes</b> format code (values 1 to 12, inclusive)</item>
-        /// <item><b>4 bytes</b> component count</item>
-        /// <item><b>4 bytes</b> inline value, or offset pointer if too large to fit in four bytes</item>
+        ///   <item><b>2 bytes</b> tag type</item>
+        ///   <item><b>2 bytes</b> format code (values 1 to 12, inclusive)</item>
+        ///   <item><b>4 bytes</b> component count</item>
+        ///   <item><b>4 bytes</b> inline value, or offset pointer if too large to fit in four bytes</item>
         /// </list>
         /// </remarks>
         /// <param name="handler">the <see cref="ITiffHandler"/> that will coordinate processing and accept read values</param>
@@ -101,9 +104,8 @@ namespace MetadataExtractor.Formats.Tiff
             {
                 // check for directories we've already visited to avoid stack overflows when recursive/cyclic directory structures exist
                 if (processedIfdOffsets.Contains(ifdOffset))
-                {
                     return;
-                }
+
                 // remember that we've visited this directory so that we don't visit it again later
                 processedIfdOffsets.Add(ifdOffset);
                 if (ifdOffset >= reader.Length || ifdOffset < 0)
@@ -111,6 +113,7 @@ namespace MetadataExtractor.Formats.Tiff
                     handler.Error("Ignored IFD marked to start outside data segment");
                     return;
                 }
+
                 // First two bytes in the IFD are the number of tags in this directory
                 int dirTagCount = reader.GetUInt16(ifdOffset);
                 var dirLength = (2 + (12 * dirTagCount) + 4);
@@ -119,6 +122,7 @@ namespace MetadataExtractor.Formats.Tiff
                     handler.Error("Illegally sized IFD");
                     return;
                 }
+
                 //
                 // Handle each tag in this directory
                 //
@@ -126,8 +130,10 @@ namespace MetadataExtractor.Formats.Tiff
                 for (var tagNumber = 0; tagNumber < dirTagCount; tagNumber++)
                 {
                     var tagOffset = CalculateTagOffset(ifdOffset, tagNumber);
+
                     // 2 bytes for the tag id
                     int tagId = reader.GetUInt16(tagOffset);
+
                     // 2 bytes for the format code
                     int formatCode = reader.GetUInt16(tagOffset + 2);
                     var format = TiffDataFormat.FromTiffFormatCode(formatCode);
@@ -144,6 +150,7 @@ namespace MetadataExtractor.Formats.Tiff
                         }
                         continue;
                     }
+
                     // 4 bytes dictate the number of components in this tag's data
                     var componentCount = reader.GetInt32(tagOffset + 4);
                     if (componentCount < 0)
@@ -151,6 +158,7 @@ namespace MetadataExtractor.Formats.Tiff
                         handler.Error("Negative TIFF tag component count");
                         continue;
                     }
+
                     var byteCount = componentCount * format.ComponentSizeBytes;
                     int tagValueOffset;
                     if (byteCount > 4)
@@ -170,11 +178,13 @@ namespace MetadataExtractor.Formats.Tiff
                         // 4 bytes or less and value is in the dir entry itself.
                         tagValueOffset = tagOffset + 8;
                     }
+
                     if (tagValueOffset < 0 || tagValueOffset > reader.Length)
                     {
                         handler.Error("Illegal TIFF tag pointer offset");
                         continue;
                     }
+
                     // Check that this tag isn't going to allocate outside the bounds of the data array.
                     // This addresses an uncommon OutOfMemoryError.
                     if (byteCount < 0 || tagValueOffset + byteCount > reader.Length)
@@ -182,6 +192,7 @@ namespace MetadataExtractor.Formats.Tiff
                         handler.Error("Illegal number of bytes for TIFF tag data: " + byteCount);
                         continue;
                     }
+
                     //
                     // Special handling for tags that point to other IFDs
                     //
@@ -190,20 +201,19 @@ namespace MetadataExtractor.Formats.Tiff
                         var subDirOffset = tiffHeaderOffset + reader.GetInt32(tagValueOffset);
                         ProcessIfd(handler, reader, processedIfdOffsets, subDirOffset, tiffHeaderOffset);
                     }
-                    else
+                    else if (!handler.CustomProcessTag(tagValueOffset, processedIfdOffsets, tiffHeaderOffset, reader, tagId, byteCount))
                     {
-                        if (!handler.CustomProcessTag(tagValueOffset, processedIfdOffsets, tiffHeaderOffset, reader, tagId, byteCount))
-                        {
-                            ProcessTag(handler, tagId, tagValueOffset, componentCount, formatCode, reader);
-                        }
+                        ProcessTag(handler, tagId, tagValueOffset, componentCount, formatCode, reader);
                     }
                 }
+
                 // at the end of each IFD is an optional link to the next IFD
                 var finalTagOffset = CalculateTagOffset(ifdOffset, dirTagCount);
                 var nextIfdOffset = reader.GetInt32(finalTagOffset);
                 if (nextIfdOffset != 0)
                 {
                     nextIfdOffset += tiffHeaderOffset;
+
                     if (nextIfdOffset >= reader.Length)
                     {
                         // Last 4 bytes of IFD reference another IFD with an address that is out of bounds
@@ -218,9 +228,7 @@ namespace MetadataExtractor.Formats.Tiff
                     }
 
                     if (handler.HasFollowerIfd())
-                    {
                         ProcessIfd(handler, reader, processedIfdOffsets, nextIfdOffset, tiffHeaderOffset);
-                    }
                 }
             }
             finally
@@ -240,13 +248,11 @@ namespace MetadataExtractor.Formats.Tiff
                     handler.SetByteArray(tagId, reader.GetBytes(tagValueOffset, componentCount));
                     break;
                 }
-
                 case TiffDataFormat.CodeString:
                 {
                     handler.SetString(tagId, reader.GetNullTerminatedString(tagValueOffset, componentCount));
                     break;
                 }
-
                 case TiffDataFormat.CodeRationalS:
                 {
                     if (componentCount == 1)
@@ -257,14 +263,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new Rational[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = new Rational(reader.GetInt32(tagValueOffset + (8 * i)), reader.GetInt32(tagValueOffset + 4 + (8 * i)));
-                        }
                         handler.SetRationalArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeRationalU:
                 {
                     if (componentCount == 1)
@@ -275,14 +278,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new Rational[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = new Rational(reader.GetUInt32(tagValueOffset + (8 * i)), reader.GetUInt32(tagValueOffset + 4 + (8 * i)));
-                        }
                         handler.SetRationalArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeSingle:
                 {
                     if (componentCount == 1)
@@ -293,14 +293,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new float[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetFloat32(tagValueOffset + (i * 4));
-                        }
                         handler.SetFloatArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeDouble:
                 {
                     if (componentCount == 1)
@@ -311,9 +308,7 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new double[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetDouble64(tagValueOffset + (i * 4));
-                        }
                         handler.SetDoubleArray(tagId, array);
                     }
                     break;
@@ -329,14 +324,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new sbyte[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetSByte(tagValueOffset + i);
-                        }
                         handler.SetInt8SArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeInt8U:
                 {
                     if (componentCount == 1)
@@ -347,14 +339,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new byte[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetByte(tagValueOffset + i);
-                        }
                         handler.SetInt8UArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeInt16S:
                 {
                     if (componentCount == 1)
@@ -365,14 +354,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new short[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetInt16(tagValueOffset + (i * 2));
-                        }
                         handler.SetInt16SArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeInt16U:
                 {
                     if (componentCount == 1)
@@ -383,14 +369,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new ushort[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetUInt16(tagValueOffset + (i * 2));
-                        }
                         handler.SetInt16UArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeInt32S:
                 {
                     // NOTE 'long' in this case means 32 bit, not 64
@@ -402,14 +385,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new int[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetInt32(tagValueOffset + (i * 4));
-                        }
                         handler.SetInt32SArray(tagId, array);
                     }
                     break;
                 }
-
                 case TiffDataFormat.CodeInt32U:
                 {
                     // NOTE 'long' in this case means 32 bit, not 64
@@ -421,14 +401,11 @@ namespace MetadataExtractor.Formats.Tiff
                     {
                         var array = new uint[componentCount];
                         for (var i = 0; i < componentCount; i++)
-                        {
                             array[i] = reader.GetUInt32(tagValueOffset + (i * 4));
-                        }
                         handler.SetInt32UArray(tagId, array);
                     }
                     break;
                 }
-
                 default:
                 {
                     handler.Error(string.Format("Unknown format code {0} for tag {1}", formatCode, tagId));
