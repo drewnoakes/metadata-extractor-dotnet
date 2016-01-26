f19a401aaf8fb1ec11dc8d257121ccb257318832
Kevin Mott <kevinm@Millerslab.com>
2016-01-26 11:48:32 -0600

    Add and display 13 new Canon tags

--- MetadataExtractor/Formats/Exif/ExifDescriptorBase.cs
+++ MetadataExtractor/Formats/Exif/ExifDescriptorBase.cs
@@ -726,7 +726,7 @@ namespace MetadataExtractor.Formats.Exif
             if (!Directory.TryGetRational(ExifDirectoryBase.TagFocalPlaneXResolution, out value))
                 return null;
             var unit = GetFocalPlaneResolutionUnitDescription();
-            return value.Reciprocal.ToSimpleString() + (unit == null ? string.Empty : " " + unit.ToLower());
+            return value.ToDecimal().ToString("0.000000");
         }
 
         [CanBeNull]
@@ -736,7 +736,7 @@ namespace MetadataExtractor.Formats.Exif
             if (!Directory.TryGetRational(ExifDirectoryBase.TagFocalPlaneYResolution, out value))
                 return null;
             var unit = GetFocalPlaneResolutionUnitDescription();
-            return value.Reciprocal.ToSimpleString() + (unit == null ? string.Empty : " " + unit.ToLower());
+            return value.ToDecimal().ToString("0.000000");
         }
 
         [CanBeNull]
