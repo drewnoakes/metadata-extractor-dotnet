e89f603b0936e3676233b6fc286834b2423c1061
Drew Noakes <git@drewnoakes.com>
2016-03-23 18:56:46 +0900

    Hoist constant array to static field.

--- MetadataExtractor/DirectoryExtensions.cs
+++ MetadataExtractor/DirectoryExtensions.cs
@@ -413,6 +413,32 @@ namespace MetadataExtractor
             return ThrowValueNotPossible<DateTime>(directory, tagType);
         }
 
+        // This seems to cover all known Exif date strings
+        // Note that "    :  :     :  :  " is a valid date string according to the Exif spec (which means 'unknown date'): http://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimeoriginal.html
+        // Custom format reference: https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx
+        private static readonly string[] _datePatterns =
+        {
+            "yyyy:MM:dd HH:mm:ss.fff",
+            "yyyy:MM:dd HH:mm:ss",
+            "yyyy:MM:dd HH:mm",
+            "yyyy-MM-dd HH:mm:ss",
+            "yyyy-MM-dd HH:mm",
+            "yyyy.MM.dd HH:mm:ss",
+            "yyyy.MM.dd HH:mm",
+            "yyyy-MM-ddTHH:mm:ss.fff",
+            "yyyy-MM-ddTHH:mm:ss.ff",
+            "yyyy-MM-ddTHH:mm:ss.f",
+            "yyyy-MM-ddTHH:mm:ss",
+            "yyyy-MM-ddTHH:mm.fff",
+            "yyyy-MM-ddTHH:mm.ff",
+            "yyyy-MM-ddTHH:mm.f",
+            "yyyy-MM-ddTHH:mm",
+            "yyyy:MM:dd",
+            "yyyy-MM-dd",
+            "yyyy-MM",
+            "yyyy"
+        };
+
         /// <summary>Attempts to return the specified tag's value as a DateTime.</summary>
         /// <remarks>If the underlying value is a <see cref="string"/>, then attempts will be made to parse it.</remarks>
         /// <returns><c>true</c> if a DateTime was returned, otherwise <c>false</c>.</returns>
@@ -436,33 +462,7 @@ namespace MetadataExtractor
 
             if (s != null)
             {
-                // This seems to cover all known Exif date strings
-                // Note that "    :  :     :  :  " is a valid date string according to the Exif spec (which means 'unknown date'): http://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimeoriginal.html
-                // Custom format reference: https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx
-                var datePatterns = new[]
-                {
-                    "yyyy:MM:dd HH:mm:ss.fff",
-                    "yyyy:MM:dd HH:mm:ss",
-                    "yyyy:MM:dd HH:mm",
-                    "yyyy-MM-dd HH:mm:ss",
-                    "yyyy-MM-dd HH:mm",
-                    "yyyy.MM.dd HH:mm:ss",
-                    "yyyy.MM.dd HH:mm",
-                    "yyyy-MM-ddTHH:mm:ss.fff",
-                    "yyyy-MM-ddTHH:mm:ss.ff",
-                    "yyyy-MM-ddTHH:mm:ss.f",
-                    "yyyy-MM-ddTHH:mm:ss",
-                    "yyyy-MM-ddTHH:mm.fff",
-                    "yyyy-MM-ddTHH:mm.ff",
-                    "yyyy-MM-ddTHH:mm.f",
-                    "yyyy-MM-ddTHH:mm",
-                    "yyyy:MM:dd",
-                    "yyyy-MM-dd",
-                    "yyyy-MM",
-                    "yyyy"
-                };
-
-                if (DateTime.TryParseExact(s, datePatterns, null, DateTimeStyles.AllowWhiteSpaces, out dateTime))
+                if (DateTime.TryParseExact(s, _datePatterns, null, DateTimeStyles.AllowWhiteSpaces, out dateTime))
                     return true;
 
                 dateTime = default(DateTime);
