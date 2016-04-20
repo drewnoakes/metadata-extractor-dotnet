2ddcb5996b36c35f7cd9ab8f39ab9807923c97f5
Drew Noakes <git@drewnoakes.com>
2016-04-20 23:13:42 +0100

    Add long-running gremlin test for Exif/TIFF data.
    Mutates every byte in the array with every value, one at a time. ExifReader should never throw an exception, though it can add as many errors as it likes to the returned Directory.

---
+++ MetadataExtractor.Tests/Formats/Exif/ExifReaderGremlinTest.cs
@@ -0,0 +1,78 @@
+#region License
+//
+// Copyright 2002-2016 Drew Noakes
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// More information about this project is available at:
+//
+//    https://github.com/drewnoakes/metadata-extractor-dotnet
+//    https://drewnoakes.com/code/exif/
+//
+#endregion
+
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Jpeg;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MetadataExtractor.Tests.Formats.Exif
+{
+    public sealed class ExifReaderGremlinTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ExifReaderGremlinTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact(Skip = "Don't run on CI machines as it takes an age to complete")]
+        public void DoesntThrowNoMatterWhat()
+        {
+            RunGremlinTest("Tests/Data/withExif.jpg.app1");
+        }
+
+        private void RunGremlinTest(string filePath)
+        {
+            var exifReader = new ExifReader();
+            var sw = Stopwatch.StartNew();
+
+            var app1 = File.ReadAllBytes(filePath);
+            var segments = new[] { app1 };
+
+            for (var i = 0; i < app1.Length; i++)
+            {
+                if (i % 1000 == 0)
+                    _output.WriteLine($"{i}/{app1.Length} bytes");
+
+                var original = app1[i];
+
+                for (var b = byte.MinValue; b < byte.MaxValue; b++)
+                {
+                    app1[i] = b;
+
+                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                    exifReader.ReadJpegSegments(segments, JpegSegmentType.App1).ToList();
+                }
+
+                app1[i] = original;
+            }
+
+            _output.WriteLine($"Finished in {sw.Elapsed.TotalSeconds:#,##0.#} seconds");
+        }
+    }
+}
