0fda5d3f898aa0ffe0916ab9d096270647e11d67
Drew Noakes <git@drewnoakes.com>
2016-10-03 20:45:04 +0100

    Version 1.5.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Reflection;
 
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyFileVersion("1.5.3.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
