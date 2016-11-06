bbb400a08820cb2d9baa5b809cd05138941c0aff
Drew Noakes <git@drewnoakes.com>
2016-11-06 01:04:54 +0000

    Add PowerShell cmdlet.

---
+++ MetadataExtractor.PowerShell/ExtractMetadataCmdlet.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace MetadataExtractor.PowerShell
+{
+    [Cmdlet("Extract", "Metadata")]
+    [UsedImplicitly]
+    public sealed class ExtractMetadata : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "Path to the file to process")]
+        [ValidateNotNullOrEmpty]
+        public string FilePath { get; set; }
+
+        [Parameter(HelpMessage = "Show raw value")]
+        public SwitchParameter Raw { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            WriteVerbose($"Extracting metadata from file: {FilePath}");
+
+            var directories = ImageMetadataReader.ReadMetadata(FilePath);
+
+            if (Raw)
+            {
+                var obj = from dir in directories
+                    where !dir.IsEmpty
+                    from tag in dir.Tags
+                    select new
+                    {
+                        Directory = dir.Name,
+                        Tag = tag.Name,
+                        RawValue = dir.GetObject(tag.Type)
+                    };
+
+                WriteObject(obj.ToList());
+            }
+            else
+            {
+                var obj = from dir in directories
+                    where !dir.IsEmpty
+                    from tag in dir.Tags
+                    select new
+                    {
+                        Directory = dir.Name,
+                        Tag = tag.Name,
+                        Description = tag.Description
+                    };
+
+                WriteObject(obj.ToList());
+            }
+        }
+    }
+}
