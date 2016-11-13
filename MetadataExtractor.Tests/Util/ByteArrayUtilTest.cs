955118b161f3721c8abdf817bd2ea948092073de
Drew Noakes <git@drewnoakes.com>
2016-11-13 15:49:37 +0000

    Add ByteArrayUtil.StartsWith and test.

---
+++ MetadataExtractor.Tests/Util/ByteArrayUtilTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+using MetadataExtractor.Util;
+
+namespace MetadataExtractor.Tests.Util
+{
+    public class ByteArrayUtilTest
+    {
+        [Fact]
+        public void StartsWith()
+        {
+            var bytes = new byte[] { 0, 1, 2, 3 };
+
+            Assert.True(bytes.StartsWith(new byte[] { }));
+            Assert.True(bytes.StartsWith(new byte[] { 0 }));
+            Assert.True(bytes.StartsWith(new byte[] { 0, 1 }));
+            Assert.True(bytes.StartsWith(new byte[] { 0, 1, 2 }));
+            Assert.True(bytes.StartsWith(new byte[] { 0, 1, 2, 3 }));
+            Assert.False(bytes.StartsWith(new byte[] { 0, 1, 2, 3, 4 }));
+            Assert.False(bytes.StartsWith(new byte[] { 1 }));
+            Assert.False(bytes.StartsWith(new byte[] { 1, 2 }));
+        }
+    }
+}
---
+++ MetadataExtractor/Util/ByteArrayUtil.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+
+namespace MetadataExtractor.Util
+{
+    public static class ByteArrayUtil
+    {
+        [Pure]
+        public static bool StartsWith([NotNull] this byte[] source, [NotNull] byte[] pattern)
+        {
+            if (source.Length < pattern.Length)
+                return false;
+
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            for (var i=0; i<pattern.Length; i++)
+                if (source[i]!=pattern[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
