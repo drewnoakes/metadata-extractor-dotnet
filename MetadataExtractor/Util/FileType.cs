cfc509cfeb1f650842df6f2cb77576749c1eed72
Drew Noakes <git@drewnoakes.com>
2017-09-07 20:11:40 +0100

    Add FileType extension methods

--- MetadataExtractor/Util/FileType.cs
+++ MetadataExtractor/Util/FileType.cs
@@ -22,66 +22,211 @@
 //
 #endregion
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
 namespace MetadataExtractor.Util
 {
     /// <summary>Enumeration of supported image file formats.</summary>
     public enum FileType
     {
         /// <summary>File type is not known.</summary>
-        Unknown,
+        Unknown = 0,
 
         /// <summary>Joint Photographic Experts Group (JPEG).</summary>
-        Jpeg,
+        Jpeg = 1,
 
         /// <summary>Tagged Image File Format (TIFF).</summary>
-        Tiff,
+        Tiff = 2,
 
         /// <summary>Photoshop Document.</summary>
-        Psd,
+        Psd = 3,
 
         /// <summary>Portable Network Graphic (PNG).</summary>
-        Png,
+        Png = 4,
 
         /// <summary>Bitmap (BMP).</summary>
-        Bmp,
+        Bmp = 5,
 
         /// <summary>Graphics Interchange Format (GIF).</summary>
-        Gif,
+        Gif = 6,
 
         /// <summary>Windows Icon.</summary>
-        Ico,
+        Ico = 7,
 
         /// <summary>PiCture eXchange.</summary>
-        Pcx,
+        Pcx = 8,
 
         /// <summary>Resource Interchange File Format.</summary>
-        Riff,
+        Riff = 9,
 
         /// <summary>Sony camera raw.</summary>
-        Arw,
+        Arw = 10,
 
         /// <summary>Canon camera raw (version 1).</summary>
-        Crw,
+        Crw = 11,
 
         /// <summary>Canon camera raw (version 2).</summary>
-        Cr2,
+        Cr2 = 12,
 
         /// <summary>Nikon camera raw.</summary>
-        Nef,
+        Nef = 13,
 
         /// <summary>Olympus camera raw.</summary>
-        Orf,
+        Orf = 14,
 
         /// <summary>Fujifilm camera raw.</summary>
-        Raf,
+        Raf = 15,
 
         /// <summary>Panasonic camera raw.</summary>
-        Rw2,
+        Rw2 = 16,
 
         /// <summary>QuickTime (mov) format video.</summary>
-        QuickTime,
+        QuickTime = 17,
 
         /// <summary>Netpbm family of image formats.</summary>
-        Netpbm
+        Netpbm = 18
+    }
+
+    public static class FileTypeExtensions
+    {
+        private static readonly string[] _shortNames =
+        {
+            "Unknown",
+            "JPEG",
+            "TIFF",
+            "PSD",
+            "PNG",
+            "BMP",
+            "GIF",
+            "ICO",
+            "PCX",
+            "RIFF",
+            "ARW",
+            "CRW",
+            "CR2",
+            "NEF",
+            "ORF",
+            "RAF",
+            "RW2",
+            "QuickTime",
+            "Netpbm"
+        };
+        
+        private static readonly string[] _longNames =
+        {
+            "Unknown",
+            "Joint Photographic Experts Group",
+            "Tagged Image File Format",
+            "Photoshop Document",
+            "Portable Network Graphics",
+            "Device Independent Bitmap",
+            "Graphics Interchange Format",
+            "Windows Icon",
+            "PiCture eXchange",
+            "Resource Interchange File Format",
+            "Sony Camera Raw",
+            "Canon Camera Raw",
+            "Canon Camera Raw",
+            "Nikon Camera Raw",
+            "Olympus Camera Raw",
+            "FujiFilm Camera Raw",
+            "Panasonic Camera Raw",
+            "QuickTime",
+            "Netpbm"
+        };
+
+        [ItemCanBeNull] private static readonly string[] _mimeTypes =
+        {
+            null,
+            "image/jpeg",
+            "image/tiff",
+            "image/vnd.adobe.photoshop",
+            "image/png",
+            "image/bmp",
+            "image/gif",
+            "image/x-icon",
+            "image/x-pcx",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "video/quicktime",
+            "image/x-portable-graymap"
+        };
+
+        [ItemCanBeNull] private static readonly string[][] _extensions =
+        {
+            null,
+            new[] { "jpg", "jpeg", "jpe" },
+            new[] { "tiff", "tif" },
+            new[] { "psd" },
+            new[] { "png" },
+            new[] { "bmp" },
+            new[] { "gif" },
+            new[] { "ico" },
+            new[] { "pcx" },
+            null,
+            new[] { "arw" },
+            new[] { "crw" },
+            new[] { "cr2" },
+            new[] { "nef" },
+            new[] { "orf" },
+            new[] { "raf" },
+            new[] { "rw2" },
+            new[] { "mov" },
+            new[] { "pbm", "ppm" }
+        };
+        
+        [NotNull]
+        public static string GetName(this FileType fileType)
+        {
+            var i = (int)fileType;
+            if (i < 0 || i >= _shortNames.Length)
+                throw new ArgumentException($"Invalid {nameof(FileType)} enum member.", nameof(fileType));
+            return _shortNames[i];
+        }
+        
+        [NotNull]
+        public static string GetLongName(this FileType fileType)
+        {
+            var i = (int)fileType;
+            if (i < 0 || i >= _longNames.Length)
+                throw new ArgumentException($"Invalid {nameof(FileType)} enum member.", nameof(fileType));
+            return _longNames[i];
+        }
+        
+        [CanBeNull]
+        public static string GetMimeType(this FileType fileType)
+        {
+            var i = (int)fileType;
+            if (i < 0 || i >= _mimeTypes.Length)
+                throw new ArgumentException($"Invalid {nameof(FileType)} enum member.", nameof(fileType));
+            return _mimeTypes[i];
+        }
+        
+        [CanBeNull]
+        public static string GetCommonExtension(this FileType fileType)
+        {
+            var i = (int)fileType;
+            if (i < 0 || i >= _extensions.Length)
+                throw new ArgumentException($"Invalid {nameof(FileType)} enum member.", nameof(fileType));
+            return _extensions[i]?.FirstOrDefault();
+        }
+        
+        [CanBeNull]
+        public static IEnumerable<string> GetAllExtensions(this FileType fileType)
+        {
+            var i = (int)fileType;
+            if (i < 0 || i >= _mimeTypes.Length)
+                throw new ArgumentException($"Invalid {nameof(FileType)} enum member.", nameof(fileType));
+            return _extensions[i];
+        }
     }
 }
