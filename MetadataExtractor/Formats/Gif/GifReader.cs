6207aa5ac7563c15757440c28233f4e35b4c4714
kevinm <kevinm@millerslab.com>
2018-10-13 15:51:14 -0500

    port #367 from Java

--- MetadataExtractor/Formats/Gif/GifReader.cs
+++ MetadataExtractor/Formats/Gif/GifReader.cs
@@ -105,12 +105,14 @@ namespace MetadataExtractor.Formats.Gif
                 {
                     case (byte)'!': // 0x21
                     {
-                        yield return ReadGifExtensionBlock(reader);
+                        var extBlock = ReadGifExtensionBlock(reader);
+                        if (extBlock != null) yield return extBlock;
                         break;
                     }
                     case (byte)',': // 0x2c
                     {
-                        yield return ReadImageBlock(reader);
+                        var imageBlock = ReadImageBlock(reader);
+                        if (imageBlock != null) yield return imageBlock;
 
                         // skip image data blocks
                         SkipBlocks(reader);
@@ -286,7 +288,11 @@ namespace MetadataExtractor.Formats.Gif
                 {
                     // XMP data extension
                     var xmpBytes = GatherBytes(reader);
-                    return new XmpReader().Extract(xmpBytes, 0, xmpBytes.Length - 257);
+                    int xmpLength = xmpBytes.Length - 257; // Exclude the "magic trailer", see XMP Specification Part 3, 1.1.2 GIF
+                    // Only extract valid blocks
+                    return xmpLength > 0
+                        ? new XmpReader().Extract(xmpBytes, 0, xmpBytes.Length - 257)
+                        : null;
                 }
                 case "ICCRGBG1012":
                 {
