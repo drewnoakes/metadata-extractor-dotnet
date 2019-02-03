78bb8e61a687e3241e1cbc905134c46e0985b52a
kevinm <kevinm@millerslab.com>
2019-02-03 15:36:57 -0600

    undo PR #135 commits in master

--- MetadataExtractor/Formats/Xmp/XmpReader.cs
+++ MetadataExtractor/Formats/Xmp/XmpReader.cs
@@ -22,7 +22,6 @@
 //
 #endregion
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -79,23 +78,14 @@ namespace MetadataExtractor.Formats.Xmp
                 var buffer = new MemoryStream();
                 foreach (var segment in extensionGroup)
                 {
-                    // TODO: re-evaluate this method, and maybe use a pre-parse strategy instead
-                    // Sometimes the byte arrays contain nul (0x00) characters. These can be legal if it's UTF-16 encoded,
-                    // but should expect UTF-8. Otherwise, these characters are illegal in XML and C#'s XmlReader will not 
-                    // handle or ignore them.
-                    // Replace nul (0x00) bytes with space character (0x20)
-                    /*for (int i = 0; i < segment.Bytes.Length; i++)
-                    {
-                        if (!IsLegalXmlChar(segment.Bytes[i]))
-                            segment.Bytes[i] = 0x20;
-                    }*/
-
                     var N = JpegSegmentPreambleExtensionBytes.Length + 32 + 4 + 4;
                     buffer.Write(segment.Bytes, N, segment.Bytes.Length - N);
                 }
 
+                buffer.Position = 0;
                 var directory = new XmpDirectory();
-                directory.Set(XmpDirectory.TagXmpByteArray, buffer.ToArray());
+                var xmpMeta = XmpMetaFactory.Parse(buffer);
+                directory.SetXmpMeta(xmpMeta);
                 directories.Add(directory);
             }
 
@@ -116,52 +106,14 @@ namespace MetadataExtractor.Formats.Xmp
             var directory = new XmpDirectory();
             try
             {
-                // TODO: re-evaluate this method, and maybe use a pre-parse strategy instead
-                // Sometimes the byte arrays contain nul (0x00) characters. These can be legal if it's UTF-16 encoded,
-                // but should expect UTF-8. Otherwise, these characters are illegal in XML and C#'s XmlReader will not 
-                // handle or ignore them.
-                // Replace nul (0x00) bytes with space character (0x20)
-                /*for (int i = 0; i < xmpBytes.Length; i++)
-                {
-                    if (!IsLegalXmlChar(xmpBytes[i]))
-                        xmpBytes[i] = 0x20;
-                }*/
-
-                if (offset == 0 && length == xmpBytes.Length)
-                    directory.Set(XmpDirectory.TagXmpByteArray, xmpBytes);
-                else
-                {
-                    var destArray = new byte[length];
-                    Array.Copy(xmpBytes, offset, destArray, 0, length);
-                    directory.Set(XmpDirectory.TagXmpByteArray, destArray);
-                }
+                var xmpMeta = XmpMetaFactory.ParseFromBuffer(xmpBytes, offset, length);
+                directory.SetXmpMeta(xmpMeta);
             }
             catch (XmpException e)
             {
                 directory.AddError("Error processing XMP data: " + e.Message);
             }
             return directory;
-        }
-
-        /// <summary>
-        /// Borrowed from:  https://seattlesoftware.wordpress.com/2008/09/11/hexadecimal-value-0-is-an-invalid-character/
-        /// </summary>
-        /// <param name="character"></param>
-        /// <returns></returns>
-        private bool IsLegalXmlChar(int character)
-        {
-            return
-            (
-                character != 0x00
-
-                // TODO: Might expand the list in the future if other bad characters appear
-                // character == 0x9 /* == '\t' == 9   */          ||
-                // character == 0xA /* == '\n' == 10  */          ||
-                // character == 0xD /* == '\r' == 13  */          ||
-                //(character >= 0x20 && character <= 0xD7FF)      ||
-                //(character >= 0xE000 && character <= 0xFFFD)    ||
-                //(character >= 0x10000 && character <= 0x10FFFF)
-            );
         }
     }
 }
