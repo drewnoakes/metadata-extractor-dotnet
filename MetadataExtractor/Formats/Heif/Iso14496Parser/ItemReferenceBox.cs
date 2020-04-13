939af2bafaa0890c4dbe2128e5418b314a95d978
Drew Noakes <git@drewnoakes.com>
2020-04-13 23:04:34 +1000

    Remove redundant OfType

--- MetadataExtractor/Formats/Heif/Iso14496Parser/ItemReferenceBox.cs
+++ MetadataExtractor/Formats/Heif/Iso14496Parser/ItemReferenceBox.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using System.Collections.Generic;
+#if NET35
 using System.Linq;
+#endif
 using MetadataExtractor.IO;
 
 namespace MetadataExtractor.Formats.Heif.Iso14496Parser
@@ -23,6 +25,10 @@ namespace MetadataExtractor.Formats.Heif.Iso14496Parser
             }
         }
 
+#if NET35
         public override IEnumerable<Box> Children() => Boxes.OfType<Box>();
+#else
+        public override IEnumerable<Box> Children() => Boxes;
+#endif
     }
 }
