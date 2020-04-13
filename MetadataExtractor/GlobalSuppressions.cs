2205984ce35d0228b2adfc31064965cf1b91b582
Drew Noakes <git@drewnoakes.com>
2020-04-13 22:03:10 +1000

    Use analyzer to enforce copyright headers

---
+++ MetadataExtractor/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0073:The file header does not match the required text", Justification = "Design-time file has other copyright", Scope = "namespace", Target = "~N:System.Diagnostics.CodeAnalysis")]
