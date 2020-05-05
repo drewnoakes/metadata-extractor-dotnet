ad67c907bb59669effa6d53536c19043279895a3
Drew Noakes <git@drewnoakes.com>
2020-05-05 19:02:44 +1000

    Normalise local functions
    This diff is hard to read. Changes here only move local functions to deeper scopes and reorder them.

--- MetadataExtractor/Formats/Heif/HeifMetadataReader.cs
+++ MetadataExtractor/Formats/Heif/HeifMetadataReader.cs
@@ -26,9 +26,17 @@ namespace MetadataExtractor.Formats.Heif
         {
             var directories = new List<Directory>();
 
+            //
+            // Read all boxes from the file
+            //
+
             var reader = new SequentialStreamReader(stream);
 
             var boxes = BoxReader.ReadBoxes(reader);
+
+            //
+            // Map those boxes to directories
+            //
 
             ParseQuickTimeTest();
 
@@ -36,106 +44,44 @@ namespace MetadataExtractor.Formats.Heif
             var itemRefs = (boxes.Descendant<ItemReferenceBox>()?.Boxes ?? new SingleItemTypeReferenceBox[0])
                 .Where(i => i.Type == BoxTypes.ThmbTag || i.Type == BoxTypes.CdscTag).ToList();
 
-            ParseImageProperties(primaryItem, itemRefs);
+            ParseImageProperties();
 
             if (stream.CanSeek)
             {
-                ParseItemSegments(itemRefs, reader);
+                ParseItemSegments();
             }
 
             return directories;
 
-            void ParseItemSegments(List<SingleItemTypeReferenceBox> itemRefs, SequentialStreamReader reader)
+            void ParseQuickTimeTest()
             {
-                var locations = boxes.Descendant<ItemLocationBox>();
-                var information = boxes.Descendant<ItemInformationBox>();
-
-                var segments = itemRefs.Select(
-                        i =>
-                            new
-                            {
-                                Info = information.Boxes.OfType<ItemInfoEntryBox>().First(j => j.ItemId == i.FromItemId),
-                                Location = locations.ItemLocations.First(j => j.ItemId == i.FromItemId)
-                            })
-                    .OrderBy(i => i.Location.BaseOffset);
-
-                foreach (var segment in segments)
+                if (boxes.Descendant<FileTypeBox>() is { } ftype)
                 {
-                    // Itemlocation is a complex structure that can load data in multiple extents in
-                    // different locations in different files.  It appears the Heics made by apple only use
-                    // the simplest of references.  Thus we check that the simplifying assumption is true
-                    // and then proceed with the simple case.
-                    if (segment.Location.ConstructionMethod == ConstructionMethod.FileOffset && segment.Location.ExtentList.Length == 1)
+                    var dir = new QuickTimeFileTypeDirectory();
+                    if (ftype.MajorBrand > 0)
                     {
-                        ParseSingleSegment(
-                            segment.Info.ItemType,
-                            segment.Location.BaseOffset + segment.Location.ExtentList[0].ExtentOffset,
-                            segment.Location.ExtentList[0].ExtentLength, reader);
+                        dir.Set(QuickTimeFileTypeDirectory.TagMajorBrand, ftype.MajorBrandString);
                     }
+
+                    if (ftype.MinorBrand > 0)
+                    {
+                        dir.Set(QuickTimeFileTypeDirectory.TagMinorVersion, ftype.MinorBrandString);
+                    }
+
+                    if (ftype.CompatibleBrands.Count > 0)
+                    {
+                        dir.Set(
+                            QuickTimeFileTypeDirectory.TagCompatibleBrands,
+                            string.Join(", ", ftype.CompatibleBrandStrings.ToArray()));
+                    }
+
+                    directories.Add(dir);
                 }
             }
 
-            void ParseSingleSegment(
-                uint itemType,
-                ulong extentOffset,
-                ulong extentLength,
-                SequentialStreamReader reader)
+            void ParseImageProperties()
             {
-                switch (itemType)
-                {
-                    case Hvc1Tag:
-                        ParseThumbnail(extentOffset, extentLength);
-                        break;
-                    case ExifTag:
-                        ParseExif(extentOffset, extentLength, reader);
-                        break;
-                }
-            }
-
-            void ParseExif(ulong extentOffset, ulong extentLength, SequentialStreamReader reader)
-            {
-                if ((long)extentLength + (long)extentOffset - reader.Position > reader.Available())
-                    return;
-
-                reader.Skip((long)extentOffset - reader.Position);
-                var exifBytes = GetExifBytes(extentLength, reader);
-
-                var parser = new ExifReader();
-                var dirs = parser.Extract(new ByteArrayReader(exifBytes));
-                directories.AddRange(dirs);
-            }
-
-            static byte[] GetExifBytes(ulong extentLength, SequentialStreamReader reader)
-            {
-                var headerLength = reader.GetUInt32();
-                if (headerLength >> 16 == 0x4d4d && extentLength >= 4) // "MM"
-                {
-                    var ret = new byte[extentLength];
-                    ret[0] = (byte)(headerLength >> 24 & 0xFF);
-                    ret[1] = (byte)(headerLength >> 16 & 0xFF);
-                    ret[2] = (byte)(headerLength >> 8 & 0xFF);
-                    ret[3] = (byte)(headerLength & 0xFF);
-                    reader.GetBytes(ret, 4, (int)extentLength - 4);
-                    return ret;
-                }
-
-                reader.Skip((int)headerLength);
-                var exifBytes = reader.GetBytes((int)extentLength);
-                return exifBytes;
-            }
-
-            void ParseThumbnail(ulong extentOffset, ulong extentLength)
-            {
-                var dir = new HeicThumbnailDirectory();
-                dir.Set(HeicThumbnailDirectory.TagFileOffset, extentOffset);
-                dir.Set(HeicThumbnailDirectory.TagLength, extentLength);
-                directories.Add(dir);
-            }
-
-            void ParseImageProperties(uint primaryItem, List<SingleItemTypeReferenceBox> itemRefs)
-            {
-                uint[] allPrimaryTiles = (boxes.Descendant<ItemReferenceBox>()?.Boxes ??
-                                          new SingleItemTypeReferenceBox[0])
+                uint[] allPrimaryTiles = (boxes.Descendant<ItemReferenceBox>()?.Boxes ?? new SingleItemTypeReferenceBox[0])
                     .SelectMany(i => i.FromItemId == primaryItem && i.Type == BoxTypes.DimgTag ? i.ToItemIds : new uint[0])
                     .ToArray();
                 var itemPropertyBox = boxes.Descendant<ItemPropertyBox>();
@@ -190,19 +136,19 @@ namespace MetadataExtractor.Formats.Heif
                         switch (prop)
                         {
                             case ImageSpatialExtentsBox ipse:
-                                ParseImageSize(dir, ipse);
+                                ParseImageSize(ipse);
                                 break;
                             case ImageRotationBox irot:
-                                ParseImageRotation(dir, irot);
+                                ParseImageRotation(irot);
                                 break;
                             case PixelInformationBox pixi:
-                                ParsePixelDepth(dir, pixi);
+                                ParsePixelDepth(pixi);
                                 break;
                             case DecoderConfigurationBox hvcC:
-                                ParseDecoderInformation(dir, hvcC);
+                                ParseDecoderInformation(hvcC);
                                 break;
                             case ColorInformationBox colr:
-                                ParseColorBox(dir, colr);
+                                ParseColorBox(colr);
                                 break;
                             default:
                                 continue;
@@ -215,85 +161,147 @@ namespace MetadataExtractor.Formats.Heif
                     {
                         directories.Remove(dir);
                     }
+
+                    return;
+
+                    void ParseImageSize(ImageSpatialExtentsBox ipse)
+                    {
+                        dir.Set(HeicImagePropertiesDirectory.TagImageWidth, ipse.X);
+                        dir.Set(HeicImagePropertiesDirectory.TagImageHeight, ipse.Y);
+                    }
+
+                    void ParseImageRotation(ImageRotationBox irot)
+                    {
+                        dir.Set(HeicImagePropertiesDirectory.TagRotation, irot.Rotation);
+                    }
+
+                    void ParsePixelDepth(PixelInformationBox pixi)
+                    {
+                        dir.Set(HeicImagePropertiesDirectory.TagPixelDepths, pixi.BitsPerChannel);
+                    }
+
+                    void ParseDecoderInformation(DecoderConfigurationBox hvcC)
+                    {
+                        dir.Set(HeicImagePropertiesDirectory.TagConfigurationVersion, hvcC.ConfigurationVersion);
+                        dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileSpace, hvcC.GeneralProfileSpace);
+                        dir.Set(HeicImagePropertiesDirectory.TagGeneralTierTag, hvcC.GeneralTierTag);
+                        dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileIdc, hvcC.GeneralProfileIdc);
+                        dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileCompatibilityTag, hvcC.GeneralProfileCompatibilityFlags);
+                        dir.Set(HeicImagePropertiesDirectory.TagGeneralLevelIdc, hvcC.GeneralLevelIdc);
+                        dir.Set(HeicImagePropertiesDirectory.TagMinSpacialSegmentationIdc, hvcC.MinSpacialSegmentationIdc);
+                        dir.Set(HeicImagePropertiesDirectory.TagParallelismType, hvcC.ParallelismType);
+                        dir.Set(HeicImagePropertiesDirectory.TagChromaFormat, hvcC.ChromaFormat);
+                        dir.Set(HeicImagePropertiesDirectory.TagBitDepthLuma, hvcC.BitDepthLumaMinus8 + 8);
+                        dir.Set(HeicImagePropertiesDirectory.TagBitDepthChroma, hvcC.BitDepthChromaMinus8 + 8);
+                        dir.Set(HeicImagePropertiesDirectory.TagAverageFrameRate, hvcC.AvgFrameRate);
+                        dir.Set(HeicImagePropertiesDirectory.TagConstantFrameRate, hvcC.ConstantFrameRate);
+                        dir.Set(HeicImagePropertiesDirectory.TagNumTemporalLayers, hvcC.NumTemporalLayers);
+                        dir.Set(HeicImagePropertiesDirectory.TagLengthSize, hvcC.LengthSizeMinus1 + 1);
+                    }
+
+                    void ParseColorBox(ColorInformationBox colr)
+                    {
+                        dir.Set(HeicImagePropertiesDirectory.TagColorFormat, colr.ColorType);
+                        if (colr.ColorType == ColorInformationBox.NclxTag)
+                        {
+                            dir.Set(HeicImagePropertiesDirectory.TagColorPrimaries, colr.ColorPrimaries);
+                            dir.Set(HeicImagePropertiesDirectory.TagColorTransferCharacteristics, colr.TransferCharacteristics);
+                            dir.Set(HeicImagePropertiesDirectory.TagColorMatrixCharacteristics, colr.MatrixCharacteristics);
+                            dir.Set(HeicImagePropertiesDirectory.TagFullRangeColor, colr.FullRangeFlag);
+                        }
+                        else
+                        {
+                            var iccDirectory = new IccReader().Extract(new ByteArrayReader(colr.IccProfile));
+                            iccDirectory.Parent = dir;
+                            directories.Add(iccDirectory);
+                        }
+                    }
                 }
             }
 
-            void ParseColorBox(HeicImagePropertiesDirectory dir, ColorInformationBox colr)
+            void ParseItemSegments()
             {
-                dir.Set(HeicImagePropertiesDirectory.TagColorFormat, colr.ColorType);
-                if (colr.ColorType == ColorInformationBox.NclxTag)
+                var locations = boxes.Descendant<ItemLocationBox>();
+                var information = boxes.Descendant<ItemInformationBox>();
+
+                var segments = itemRefs.Select(
+                        i =>
+                            new
+                            {
+                                Info = information.Boxes.OfType<ItemInfoEntryBox>().First(j => j.ItemId == i.FromItemId),
+                                Location = locations.ItemLocations.First(j => j.ItemId == i.FromItemId)
+                            })
+                    .OrderBy(i => i.Location.BaseOffset);
+
+                foreach (var segment in segments)
                 {
-                    dir.Set(HeicImagePropertiesDirectory.TagColorPrimaries, colr.ColorPrimaries);
-                    dir.Set(HeicImagePropertiesDirectory.TagColorTransferCharacteristics, colr.TransferCharacteristics);
-                    dir.Set(HeicImagePropertiesDirectory.TagColorMatrixCharacteristics, colr.MatrixCharacteristics);
-                    dir.Set(HeicImagePropertiesDirectory.TagFullRangeColor, colr.FullRangeFlag);
+                    // ItemLocationBox is a complex structure that can load data in multiple extents in
+                    // different locations in different files. It appears the HEICs made by Apple only use
+                    // the simplest of references. Thus we check that the simplifying assumption is true
+                    // and then proceed with the simple case.
+                    if (segment.Location.ConstructionMethod == ConstructionMethod.FileOffset && segment.Location.ExtentList.Length == 1)
+                    {
+                        ParseSingleSegment(
+                            segment.Info.ItemType,
+                            segment.Location.BaseOffset + segment.Location.ExtentList[0].ExtentOffset,
+                            segment.Location.ExtentList[0].ExtentLength);
+                    }
                 }
-                else
+
+                return;
+
+                void ParseSingleSegment(uint itemType, ulong extentOffset, ulong extentLength)
                 {
-                    var iccDirectory = new IccReader().Extract(new ByteArrayReader(colr.IccProfile));
-                    iccDirectory.Parent = dir;
-                    directories.Add(iccDirectory);
-                }
-            }
-
-            static void ParseDecoderInformation(HeicImagePropertiesDirectory dir, DecoderConfigurationBox hvcC)
-            {
-                dir.Set(HeicImagePropertiesDirectory.TagConfigurationVersion, hvcC.ConfigurationVersion);
-                dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileSpace, hvcC.GeneralProfileSpace);
-                dir.Set(HeicImagePropertiesDirectory.TagGeneralTierTag, hvcC.GeneralTierTag);
-                dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileIdc, hvcC.GeneralProfileIdc);
-                dir.Set(HeicImagePropertiesDirectory.TagGeneralProfileCompatibilityTag, hvcC.GeneralProfileCompatibilityFlags);
-                dir.Set(HeicImagePropertiesDirectory.TagGeneralLevelIdc, hvcC.GeneralLevelIdc);
-                dir.Set(HeicImagePropertiesDirectory.TagMinSpacialSegmentationIdc, hvcC.MinSpacialSegmentationIdc);
-                dir.Set(HeicImagePropertiesDirectory.TagParallelismType, hvcC.ParallelismType);
-                dir.Set(HeicImagePropertiesDirectory.TagChromaFormat, hvcC.ChromaFormat);
-                dir.Set(HeicImagePropertiesDirectory.TagBitDepthLuma, hvcC.BitDepthLumaMinus8 + 8);
-                dir.Set(HeicImagePropertiesDirectory.TagBitDepthChroma, hvcC.BitDepthChromaMinus8 + 8);
-                dir.Set(HeicImagePropertiesDirectory.TagAverageFrameRate, hvcC.AvgFrameRate);
-                dir.Set(HeicImagePropertiesDirectory.TagConstantFrameRate, hvcC.ConstantFrameRate);
-                dir.Set(HeicImagePropertiesDirectory.TagNumTemporalLayers, hvcC.NumTemporalLayers);
-                dir.Set(HeicImagePropertiesDirectory.TagLengthSize, hvcC.LengthSizeMinus1 + 1);
-            }
-
-            static void ParsePixelDepth(HeicImagePropertiesDirectory dir, PixelInformationBox pixi)
-            {
-                dir.Set(HeicImagePropertiesDirectory.TagPixelDepths, pixi.BitsPerChannel);
-            }
-
-            static void ParseImageRotation(HeicImagePropertiesDirectory dir, ImageRotationBox irot)
-            {
-                dir.Set(HeicImagePropertiesDirectory.TagRotation, irot.Rotation);
-            }
-
-            static void ParseImageSize(HeicImagePropertiesDirectory dir, ImageSpatialExtentsBox ipse)
-            {
-                dir.Set(HeicImagePropertiesDirectory.TagImageWidth, ipse.X);
-                dir.Set(HeicImagePropertiesDirectory.TagImageHeight, ipse.Y);
-            }
-
-            void ParseQuickTimeTest()
-            {
-                if (boxes.Descendant<FileTypeBox>() is { } ftype)
-                {
-                    var dir = new QuickTimeFileTypeDirectory();
-                    if (ftype.MajorBrand > 0)
+                    switch (itemType)
                     {
-                        dir.Set(QuickTimeFileTypeDirectory.TagMajorBrand, ftype.MajorBrandString);
+                        case Hvc1Tag:
+                            ParseThumbnail();
+                            break;
+                        case ExifTag:
+                            ParseExif();
+                            break;
                     }
 
-                    if (ftype.MinorBrand > 0)
+                    return;
+
+                    void ParseThumbnail()
                     {
-                        dir.Set(QuickTimeFileTypeDirectory.TagMinorVersion, ftype.MinorBrandString);
+                        var dir = new HeicThumbnailDirectory();
+                        dir.Set(HeicThumbnailDirectory.TagFileOffset, extentOffset);
+                        dir.Set(HeicThumbnailDirectory.TagLength, extentLength);
+                        directories.Add(dir);
                     }
 
-                    if (ftype.CompatibleBrands.Count > 0)
+                    void ParseExif()
                     {
-                        dir.Set(
-                            QuickTimeFileTypeDirectory.TagCompatibleBrands,
-                            string.Join(", ", ftype.CompatibleBrandStrings.ToArray()));
-                    }
+                        if ((long)extentLength + (long)extentOffset - reader.Position > reader.Available())
+                            return;
 
-                    directories.Add(dir);
+                        reader.Skip((long)extentOffset - reader.Position);
+                        var exifBytes = GetExifBytes();
+
+                        var parser = new ExifReader();
+                        var dirs = parser.Extract(new ByteArrayReader(exifBytes));
+                        directories.AddRange(dirs);
+
+                        byte[] GetExifBytes()
+                        {
+                            var headerLength = reader.GetUInt32();
+                            if (headerLength >> 16 == 0x4d4d && extentLength >= 4) // "MM"
+                            {
+                                var ret = new byte[extentLength];
+                                ret[0] = (byte)(headerLength >> 24 & 0xFF);
+                                ret[1] = (byte)(headerLength >> 16 & 0xFF);
+                                ret[2] = (byte)(headerLength >> 8 & 0xFF);
+                                ret[3] = (byte)(headerLength & 0xFF);
+                                reader.GetBytes(ret, 4, (int)extentLength - 4);
+                                return ret;
+                            }
+
+                            reader.Skip((int)headerLength);
+                            return reader.GetBytes((int)extentLength);
+                        }
+                    }
                 }
             }
         }
