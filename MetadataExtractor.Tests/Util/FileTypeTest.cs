a26fc490ce10fba65edee31f450e700dbdd895f3
Drew Noakes <git@drewnoakes.com>
2020-05-05 19:50:50 +1000

    Add FileTypeTest

---
+++ MetadataExtractor.Tests/Util/FileTypeTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MetadataExtractor.Util;
+using Xunit;
+
+namespace MetadataExtractor.Tests.Util
+{
+    public class FileTypeTest
+    {
+        [Theory]
+        [MemberData(nameof(FileTypeMembers))]
+        public void GetName(FileType member)
+        {
+            member.GetName();
+        }
+
+        [Theory]
+        [MemberData(nameof(FileTypeMembers))]
+        public void GetLongName(FileType member)
+        {
+            member.GetLongName();
+        }
+
+        [Theory]
+        [MemberData(nameof(FileTypeMembers))]
+        public void GetMimeType(FileType member)
+        {
+            member.GetMimeType();
+        }
+
+        [Theory]
+        [MemberData(nameof(FileTypeMembers))]
+        public void GetCommonExtension(FileType member)
+        {
+            member.GetCommonExtension();
+        }
+
+        [Theory]
+        [MemberData(nameof(FileTypeMembers))]
+        public void GetAllExtensions(FileType member)
+        {
+            member.GetAllExtensions();
+        }
+
+        private static IEnumerable<object[]> FileTypeMembers()
+        {
+            return Enum.GetValues(typeof(FileType)).Cast<FileType>().Select(fileType => new object[] { fileType });
+        }
+    }
+}
