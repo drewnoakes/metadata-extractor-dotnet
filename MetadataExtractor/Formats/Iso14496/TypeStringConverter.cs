3a874394e27a438f22c09f1694870323dd390117
Drew Noakes <git@drewnoakes.com>
2020-07-05 20:45:41 +1000

    Add TypeStringConverter.ToTypeId

--- MetadataExtractor/Formats/Iso14496/TypeStringConverter.cs
+++ MetadataExtractor/Formats/Iso14496/TypeStringConverter.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System;
+
 namespace MetadataExtractor.Formats.Iso14496
 {
     internal static class TypeStringConverter
@@ -14,6 +16,20 @@ namespace MetadataExtractor.Formats.Iso14496
                     (char)((input >> 8) & 0xFF),
                     (char)(input & 0xFF)
                 });
+        }
+
+        public static uint ToTypeId(string typeString)
+        {
+            if (typeString.Length != 4)
+            {
+                throw new ArgumentException("Must be four characters long.", nameof(typeString));
+            }
+
+            return (uint)
+                (((typeString[0] & 0xFF) << 24) |
+                 ((typeString[1] & 0xFF) << 16) |
+                 ((typeString[2] & 0xFF) << 8) |
+                 ( typeString[3] & 0xFF));
         }
     }
 }
